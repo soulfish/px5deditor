46b4d4971b821b39c95594f0939bdd01e6159e0b
Frédéric Jaume <fred@exp-media.com>
2018-06-12 22:26:51 +0200

    Fix an icon path issue

--- px5de_desktop.sh
+++ px5de_desktop.sh
@@ -2,12 +2,12 @@
 cat <<EOF
 [Desktop Entry]
 Name=PX5D Editor for Linux
-Version=0.2
+Version=0.3
 GenericName=Preset Editor
 Comment=This is a preset editor for the Korg Pandora PX5D
 Exec=$1/bin/px5deditor &
 TryExec=$1/bin/px5deditor
-Icon=px5deditor
+Icon=/usr/share/icons/hicolor/48x48/apps/px5deditor.png
 Categories=Application;AudioVideo;Audio;Midi;X-Alsa;Qt
 Terminal=false
 Type=Application
